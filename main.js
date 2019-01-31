d041a700513f215a9bdbbc20cd49533cf8792392
Mengqi Wang <wmq516@gmail.com>
2019-01-30 22:06:48 -0700

    added variable  yearOfBirth

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function createGreeting(name, age){
-    return `Hi, My name is ${name} and I am ${age} years old.`;
+    const yearOfBirth = 2019 - age;
+    return `Hi, My name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting = createGreeting('Kyler', 24);
